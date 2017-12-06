bd68fed8550c06fb18a7b7930a04bd20a4cd38a4
adigozellilamia <lamiye.adigozelli@icloud.com>
2017-12-06 08:29:52 +0400

    change slider

--- Danger/js/script.js
+++ Danger/js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
-    $('.loop').owlCarousel({
+    $('.owl-carousel').owlCarousel({
         center: true,
-        items:2,
+        // autoplay:true,
+        autoplayTimeout:4000,
+        items:3,
         loop:true,
+        dots:false,
         margin:10,
         responsive:{
+            0:{
+                items:1
+            },
             600:{
-                items:4
+                items:2
+            },
+            1000:{
+                items:2
             }
         }
     });
